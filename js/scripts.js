40a5a33ffde13182619dc299ffb6b271c8a9e67a
MaryMonari <moraamarymonari@gmail.com>
2017-05-16 20:32:16 +0300

    edited navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,1 @@
-// Validation
-$(document).ready(function(){
-// function validateForm(){
-  // for(var i=0; i< myform.elements.length; i++){
-//     if(myform.elements[i].className == "req" && myform.elements[i].value.length == 0){
-//       alert("Please fill in all required fields");
-//       return false;
-//     }
-// };
-$("button").click(function(){
-        var x = $("form").serializeArray();
-        $.each(x, function(i, field){
-            $("#results").append(field.name + ":" + field.value + " ");
-        });
-    });
-});
+
--- js/scripts3.js
+++ js/scripts3.js
@@ -2,42 +2,54 @@ $(document).ready(function(){
  $("#button").click(function(){
         var x = $("radio").serializeArray();
        $.each(x, function(i, field){
-           $("#results").append(name.value);
+           $("#results").show(name.value);
 
        });
         event.preventDefault();
           });
+          $.fn.serializeObject = function () {
+          var o = {};
+          var a = this.serializeArray();
+          $.each(a, function () {
+              if (o[this.name] !== undefined) {
+                  if (!o[this.name].push) {
+                      o[this.name] = [o[this.name]];
+                  }
+                  o[this.name].push(this.value || "");
+              } else {
+                  o[this.name] = this.value || "";
+              }
+          });
+          var $radio = $('input[type=radio],input[type=checkbox]',this);
+          $.each($radio,function(){
+              if(!o.hasOwnProperty(this.name)){
+                  o[this.name] = "";
+              }
+          });
+          return o;
+          };
+          event.preventDefault();
 
+       var results = [o];
+       var arrayLength = results.length;
+       var roundNumber= 0;
+       var msg= "";
+       var i;
 
-      // $.fn.serializeObject = function () {
-      // var o = {};
-      // var a = this.serializeArray();
-      // $.each(a, function () {
-      //     if (o[this.name] !== undefined) {
-      //         if (!o[this.name].push) {
-      //             o[this.name] = [o[this.name]];
-      //         }
-      //         o[this.name].push(this.value || '');
-      //     } else {
-      //         o[this.name] = this.value || '';
-      //     }
-      // });
-      // var $radio = $('input[type=radio],input[type=checkbox]',this);
-      // $.each($radio,function(){
-      //     if(!o.hasOwnProperty(this.name)){
-      //         o[this.name] = '';
-      //     }
-      // });
-      // return o;
-      // };
-      // event.preventDefault();
+       for(i=o; i<arrayLength; i++) {
+         roundNumber = (i+1);
+         msg += "Round" +  roundNumber + ":";
+         msg += scores [i] + "<br/>";
+       }
 
-      var arr = $("radio").serializeArray();
-      var len = arr.length;
-      var dataObj = {};
-      for (i=0; i<len; i++) {
-        dataObj [arr [i].name]= [arr[i].value];
-      }
-      .console.log(dataObj ["name"]);
-      .show(dataObj ["value"]);
+       document.getElementById ("results").innerHTML = msg;
+
+      // var arr = $("radio").serializeArray();
+      // var len = arr.length;
+      // var dataObj = {};
+      // for (i=0; i<len; i++) {
+      //   dataObj [arr [i].name]= [arr[i].value];
+      // }
+      // .console.log(dataObj ["name"]);
+      // .show(dataObj ["value"]);
 });
